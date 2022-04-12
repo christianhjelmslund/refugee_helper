cba1e2f4434785d2192757e8401c4aaf2a2cca5f
sasp1 <sebastian.a.specht@gmail.com>
2022-04-12 19:09:13 +0200

    Added update to siddhi file

--- crisis_detector.py
+++ crisis_detector.py
@@ -3,6 +3,8 @@ import requests
 import json
 from newsapi import NewsApiClient
 import sys
+import random
+
 
 NEWS_API_KEY = '93ff7e0e52884b04824ac8f584c56991'
 SIDDHI_URL = 'http://localhost:8006'
@@ -23,17 +25,22 @@ top_headlines = requests.get('https://newsapi.org/v2/top-headlines?q=war&languag
 import  time
 #
 while True:
-    news = requests.get('https://newsapi.org/v2/everything?q=war+ukraine&language=en&from=2022-04-05', headers=news_headers).json()
+    country = "ukraine"
+    news = requests.get('https://newsapi.org/v2/everything?q=war+' + country + '&language=en&from=2022-04-05', headers=news_headers).json()
     # print(top_headlines)
     print(news["totalResults"])
-    if news["totalResults"] > 10:
-        print(news["totalResults"])
+    news_count = news["totalResults"] + random.randint(0,50)
+    data = '{"event":{"news_count":' + str(news_count) + ', "country": "' + country + '"}}'
+    requests.post(SIDDHI_URL+'/news', headers=siddhi_headers, data=data)
+
+
+#     if news["totalResults"] > 10:
+#         print(news["totalResults"])
 #     if (top_headlines['articles']):
 #         for article in top_headlines['articles']:
 #             title = article['title']
 #             data = '{"event":{"title":"title"}}'
 #             print(title)
-#             requests.post(SIDDHI_URL+'/init_crisis', headers=siddhi_headers, data=data)
 
     sys.stdout.flush()
     time.sleep(2)
