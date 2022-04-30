909fa863a5e9bd6432af77e1d602c4c69ad98131
sasp1 <sebastian.a.specht@gmail.com>
2022-04-30 20:13:24 +0200

    separation of visa to backend-frontend

---
+++ refugee-helper/src/main/java/com/example/workflow/visa/SendApplyInDestinationCountry.java
@@ -0,0 +1,15 @@
+package com.example.workflow.visa;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SendApplyInDestinationCountry implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        execution.getProcessEngineServices().
+                getRuntimeService().
+                createMessageCorrelation("msg_apply_in_dest_country").
+                correlateWithResult();
+
+    }
+}
