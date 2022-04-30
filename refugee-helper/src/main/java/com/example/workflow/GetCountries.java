b75b95ebc9bc1b3253e48c86f02bcf8798e83411
Christian <christianhjelmslund@gmail.com>
2022-04-30 22:21:22 +0200

    updated and getting countries from api

---
+++ refugee-helper/src/main/java/com/example/workflow/GetCountries.java
@@ -0,0 +1,53 @@
+package com.example.workflow;
+
+import com.example.workflow.models.CountriesModel;
+import com.example.workflow.models.CountryModel;
+import com.example.workflow.services.CountryService;
+import com.example.workflow.transportation.models.CitiesModel;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.variable.Variables;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.workflow.CheckUserInfo.REFUGEE_APP;
+import static org.camunda.bpm.engine.variable.Variables.objectValue;
+
+@Component
+public class GetCountries implements JavaDelegate {
+
+    @Autowired
+    private CountryService countryService;
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+        //
+        CountriesModel countries = countryService.getAllCountries();
+
+        // Map<String, String> countriesMap = new HashMap<String, String>();
+        // for(int i = 0; i < cities.getCities().size(); i++) {
+        //   citiesMap.put(cities.getCities().get(i).getName(),cities.getCities().get(i).getName());
+        // }
+
+        // REFUGEE_APP.info("Countries:" + countries.getCountries());
+
+        Map<String, CountryModel> countriesMap = new HashMap<>();
+        for(int i = 0; i < countries.getCountries().size(); i++) {
+            countriesMap.put(countries.getCountries().get(i).getName(),countries.getCountries().get(i));
+        }
+        execution.setVariable("countries",
+                objectValue(countriesMap)
+                        .serializationDataFormat(Variables.SerializationDataFormats.JSON)
+                        .create());
+
+        REFUGEE_APP.info("Countries:" + execution.getVariable("countries"));
+
+        // execution.setVariable("countries", countryService.getAllCountries().getCountries());
+
+    }
+
+}
