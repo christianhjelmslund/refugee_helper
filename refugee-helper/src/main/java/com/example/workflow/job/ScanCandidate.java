420dc91f61918048f19a184938d84575a5d5dc86
ladenma <laurajamenda@gmail.com>
2022-05-01 13:11:47 +0200

    Update job process (incl forms)

---
+++ refugee-helper/src/main/java/com/example/workflow/job/ScanCandidate.java
@@ -0,0 +1,30 @@
+package com.example.workflow.job;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import java.util.Random;
+
+import java.util.HashMap;
+
+import static com.example.workflow.job.SendJobInterest.COMPANY;
+
+public class ScanCandidate implements JavaDelegate {
+
+    public void execute(DelegateExecution execution) throws Exception {
+        // Randomly decide if user is invited for interview
+        Random random = new Random();
+        String invite_user;
+
+        int random_int = random.nextInt(10);
+
+        if (random_int > 2){
+            invite_user = "true";
+        } else {
+            invite_user = "false";
+        }
+
+        COMPANY.info("Decision to invite user for interview: " + invite_user);
+        execution.setVariable("invite_user", invite_user);
+    }
+
+}
